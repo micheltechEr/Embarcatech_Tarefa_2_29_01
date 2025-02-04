945f5f24347824f15b30eaf87870bdedcc737510
micheltechEr <angelomiguelrib@gmail.com>
2025-02-03 23:04:52 -0300

    Adição da função do temporizador

--- Embarcatech_Tarefa_29_01.c
+++ Embarcatech_Tarefa_29_01.c
@@ -2,11 +2,43 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
+#define LED_VERMELHO 11
+#define LED_AMARELO 12
+#define LED_VERDE 13
+
+//Variável do estado do semárofo, do tipo inteiro e global
+int estado_semaforo = 0;
+
+
+bool repeating_timer_callback(struct repeating_timer *t) {
+    // Alterna entre os estados do semáforo
+    //Cada case aciona uma cor diferente do LED RGB
+    switch (estado_semaforo) {
+        case 0:
+            gpio_put(LED_VERMELHO, 1); 
+            gpio_put(LED_AMARELO, 0);
+            gpio_put(LED_VERDE, 0);
+            estado_semaforo = 1;
+            break;
+        case 1:
+            gpio_put(LED_VERMELHO, 0);
+            gpio_put(LED_AMARELO, 1); 
+            gpio_put(LED_VERDE, 0);
+            estado_semaforo = 2;
+            break;
+        case 2:
+            gpio_put(LED_VERMELHO, 0);
+            gpio_put(LED_AMARELO, 0);
+            gpio_put(LED_VERDE, 1); 
+            estado_semaforo = 0;
+            break;
+    }
+    return true; 
+}
+
 int main() {
 
     while (1) {
-        stdio_init_all();
         printf("Running");
-         sleep_ms(1000);
     }
 }
